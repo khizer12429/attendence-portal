c42c28c5f34666c2092d5b38a1ac91bfe716e72d
Khizer Farooq <khizerfarooq267@gmail.com>
2025-04-21 10:34:23 +0500

    work done in home by khizer

--- sript.js
+++ sript.js
@@ -1,13 +1,50 @@
 const show = document.getElementById("show");
-const name = document.getElementById("name").value;
-const opt = document.getElementById("opt").value;
-const date = document.getElementById("date").value;
-const nameshow = document.getElementById("nameshow").value;
-const nubershow = document.getElementById("nubershow").value;
-const dateshow = document.getElementById("dateshow").value;
-const statusshow = document.getElementById("statusshow").value;
+const names = document.getElementById("names");
+const opt = document.getElementById("opt");
+const date = document.getElementById("date");
+const nameshow = document.getElementById("nameshow");
+const numbershow = document.getElementById("numbershow");
+const dateshow = document.getElementById("dateshow");
+const statusshow = document.getElementById("statusshow");
 
-const result = () =>{
-    show.innerHTML = `<tbody id="show">${name}</tbody>`
-    console.log("hi")
-}
+let num = 1;
+let arr = JSON.parse(localStorage.getItem("arr")) || [];
+
+const result = () => {
+  let Name = names.value;
+  let option = opt.value;
+  let dates = date.value;
+  show.innerHTML += ` <td>${num}</td>
+    <td>${Name}</td>
+    <td>${option}</td>
+    <td>${dates}</td>`;
+
+  // set item
+  localStorage.setItem("name", Name);
+  localStorage.setItem("status", option);
+  localStorage.setItem("date", dates);
+
+  const student = {
+    name: Name,
+    status: option,
+    date: dates,
+  };
+
+  arr.push(student);
+  localStorage.setItem("arr", JSON.stringify(arr));
+
+};
+console.log(arr);
+
+window.onload = () => {
+    arr.forEach((student, index) => {
+      show.innerHTML += `<tr>
+        <td>${index + 1}</td>
+        <td>${student.name}</td>
+        <td>${student.status}</td>
+        <td>${student.date}</td>
+      </tr>`;
+    });
+    num = arr.length + 1; 
+  };
+  
